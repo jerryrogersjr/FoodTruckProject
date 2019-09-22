4996603d0b1f3e01ee5bc9e43d0cd2e948763815
Jerry Rogers <jerryrogersjr@gmail.com>
2019-09-21 22:21:14 -0600

    reAttack

--- src/FoodTruck.java
+++ src/FoodTruck.java
@@ -1,4 +1,99 @@
+import java.util.Scanner;
 
 public class FoodTruck {
-    // fields
+	// fields
+	private String trkName = null;
+	private String foodItem = null;
+	private int trkId = 0;
+	private int rating = 0;
+	private static int nextTruckId = 0;
+	
+	
+	// constructors
+	public FoodTruck() {
+		setTrkId(trkId);
+	}
+
+	public FoodTruck(String trkName, String foodItem, int trkId, int rating) {
+		setTrkId(trkId);
+		this.trkName = trkName;
+		this.foodItem = foodItem;
+		this.trkId = trkId;
+		this.rating = rating;
+	}
+
+	public String getTrkName() {
+		return trkName;
+	}
+
+	public void setTrkName(String trkName) {
+		this.trkName = trkName;
+	}
+
+	public String getFoodItem() {
+		return foodItem;
+	}
+
+	public void setFoodItem(String foodItem) {
+		this.foodItem = foodItem;
+	}
+
+	public int getTrkId() {
+		return trkId;
+	}
+
+	public void setTrkId(int trkId) {
+		trkId++;
+		nextTruckId = trkId;
+		this.trkId = nextTruckId;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public static void setNextTruckId(int nextTruckId) {
+		FoodTruck.nextTruckId = nextTruckId;
+		nextTruckId++;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("FoodTruck [trkName=%s, foodItem=%s, trkId=%s, rating=%s]", 
+				trkName, foodItem, trkId, rating);
+	}
+
+	public int getMenu(Scanner kb) {
+		System.out.println("Choose from the following menu");
+		System.out.println("*=======================================*");
+		System.out.println("1 <== List all Food Trucks");
+		System.out.println("3 <== Display Food Truck Average Rating");
+		System.out.println("4 <== Display Highest Rated Food Truck");
+		System.out.println("5 <== Quit Program");
+		System.out.println("*=======================================*");
+		int output = kb.nextInt();
+		return output;
+		
+	}
+
+
+
+	
+
+	
+
+
+
+
+	
+
+
+	
+	
+	
+	
 }
--- src/FoodTruckApp.java
+++ src/FoodTruckApp.java
@@ -4,32 +4,38 @@ public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		//FoodTruck trk = new FoodTruck();
-		FoodTruck[] names = new FoodTruck[5];
+		// FoodTruck trk = new FoodTruck();
 
-		System.out.print("Enter up to 5 Food Truck names.\n");
-		for (int i = 0; i < names.length; i++) {
-			names[i] = new FoodTruck();
-			System.out.print((names.length - i) + " More Truck(s): ");
-			String trk = kb.nextLine();
+		System.out.print("Enter up to 5 Food Truck names, food type, and rating.\n");
 
-			String q = "quit";
-			if (q.equalsIgnoreCase("quit")) {
-				FoodTruck[] quitArray = new FoodTruck[i];
-				for (int ii = 0; ii < quitArray.length; ii++) {
-					quitArray[ii] = names[ii];
+		FoodTruck[] ft = new FoodTruck[5];
+		String trk1 = "";
+		for (int i = 0; i < ft.length; i++) {
+			ft[i] = new FoodTruck();
+
+			if (trk1.equalsIgnoreCase("Quit")) {
+				break;
+			} else {
+				System.out.print((ft.length - i) + " Truck Name: ");
+				trk1 = kb.next();
+				ft[i].setTrkName(trk1);
+				if (trk1.equalsIgnoreCase("Quit")) {
+					break;
+				} else {
+					System.out.print((ft.length - i) + " Food Type: ");
+					String type = kb.next();
+					ft[i].setFoodItem(type);
 				}
-
+				if (trk1.equalsIgnoreCase("Quit")) {
+					break;
+				} else {
+					System.out.print((ft.length - i) + " Rating 1 to 5: ");
+					int rating = kb.nextInt();
+					ft[i].setRating(rating);
+					System.out.println();
+				}
 			}
-//			else {
-//				names[i].setTrkName();
 
-//			for (int i = 0; i < names.length; i++) {
-//				
-//				names[i] = new FoodTruck();
-//				//FoodTruck num = names[i];
-//				System.out.print("Next Truck: ");
-//				
 		}
 
 	}
